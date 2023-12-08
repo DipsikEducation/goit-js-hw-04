16403affd61b81296d2fd7ddd72bccf6ad2b4ce6
DipsikKk <141740406+DearDany@users.noreply.github.com>
2023-12-08 16:28:15 +0200

    complite tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,40 @@
 /*
-1. Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+1.Функцыя isEnoughCapacity(products, containerSize), яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
 
-2.Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
-Усі символи slug повинні бути в нижньому регістрі.
-Усі слова slug повинні бути розділені тире.
+2.
+- products — обєкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. Наприклад, { apples: 2, grapes: 4 }.
+- containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+
+3.Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість
+ товарів в об’єкті products і 
+повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
 */
 
 
-function slugify(title) {
-    const slug = title.toLowerCase().split(" ").join("-");
-    return slug
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+
+  for (let product in products) {
+    totalQuantity += products[product];
+  }
+
+  return totalQuantity <= containerSize;
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
